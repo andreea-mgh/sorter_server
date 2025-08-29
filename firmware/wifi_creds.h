5b3444f17575f0c20c2bf46a0a250976bdd435ab
Andreea Megherlich <35776499+andreea-mgh@users.noreply.github.com>
2025-08-29 21:25:30 +0300

    Add WiFi credentials and server endpoints

---
+++ firmware/wifi_creds.h
@@ -0,0 +1,9 @@
+const char* ssid = "********";
+const char* password = "********";
+
+const char* serverUrl = "http://192.168.****:5000";
+
+const char* endpointInit = "/init";
+const char* endpointUpload = "/upload";
+const char* endpointLog = "/log";
+const char* endpointResponse = "/result";
